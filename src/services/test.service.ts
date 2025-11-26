a48146a5fcefcccd843bb27f3a5ea7effef220ee
DESKTOP-9RJEA40\Srun Kerry <srunkerry@gmail.com>
2025-11-26 10:36:57 +0700

    change

--- src/services/test.service.ts
+++ src/services/test.service.ts
@@ -1,7 +1,6 @@
 import type { Context } from "hono";
 const testService = {
   get: async (c: Context) => {
-    const body = await c.req.param("id");
     return c.json({
       msg: "hello",
     });
