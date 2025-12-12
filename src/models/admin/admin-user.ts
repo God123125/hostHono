61ec911f0b7e8e2cbc24457f4da4e15cb908524f
DESKTOP-MI4T68R\msi <srunkerry@gmail.com>
2025-12-12 07:41:43 +0700

    working on admin-user

---
+++ src/models/admin/admin-user.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+import * as z from "zod";
+export const adminUser = z.object({
+  name: z.string().optional(),
+  email: z.string(),
+  password: z.string().min(8).max(15),
+  role: z.string(),
+});
+export type adminUser = z.infer<typeof adminUser>;
+const adminUserSchema = new Schema<adminUser>(
+  {
+    name: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export default mongoose.model<adminUser>("admin_users", adminUserSchema);
